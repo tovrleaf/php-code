8cf399f39e23a439e8d843a169d264130c7a2e78
Niko Kivelä <niko@tovrleaf.com>
2010-07-20 22:10:20 +0300

    Documentation to array_depth

--- include/function.array-depth.php
+++ include/function.array-depth.php
@@ -1,15 +1,20 @@
 <?php
 /**
+ * @author Niko Kivelä <niko@tovrleaf.com>
+ * @since Tue Jul 20 22:08:35 EEST 2010
+ *
+ * <code>
+ * // returns 2
+ * echo array_depth(array(1, 2, array(2)));
+ * <code>
+ */
+
+/**
  * Calculate array depth (dimension)
  *
  * Uses textual presentation of array instead of looping through it so there's
  * no consern that if array has reference to itself function will loop infinite
  * (in case of recursion).
- *
- * @code
- * // returns 2
- * echo array_depth(array(1, 2, array(2)));
- * @code
  *
  * @param array $arr  Input array, return its dimension
  * @return int   Depth of array
